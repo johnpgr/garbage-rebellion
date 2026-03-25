8b1fb335b911df5dedbcafdf7d1e4ab18a7fde41
johnpgr <jp.greidinger@hotmail.com>
2026-03-25 15:56:24 -0300

    refactor: remove west anims, add aabb collision & debug
    Remove unused west-facing animations and delete associated assets from
    sprite atlas. Refactor player controller to use custom AABB collision
    resolution for all movement and collisions (replacing physics). Add new
    AABB module and debug box component to main collection for testing
    player collision. Camera script updated for screen-by-screen scrolling.

---
+++ main/collision/aabb.lua
@@ -0,0 +1,68 @@
+local M = {}
+
+local registry = {}
+
+local function copy_v3(v)
+	return vmath.vector3(v.x, v.y, v.z)
+end
+
+local function draw_line(start_point, end_point, color)
+	msg.post("@render:", "draw_line", {
+		start_point = start_point,
+		end_point = end_point,
+		color = color,
+	})
+end
+
+function M.register(id, center, half_extents)
+	registry[id] = {
+		center = copy_v3(center),
+		half_extents = copy_v3(half_extents),
+	}
+end
+
+function M.update(id, center, half_extents)
+	if not registry[id] then
+		M.register(id, center, half_extents)
+		return
+	end
+
+	registry[id].center = copy_v3(center)
+	registry[id].half_extents = copy_v3(half_extents)
+end
+
+function M.unregister(id)
+	registry[id] = nil
+end
+
+function M.get_boxes()
+	return registry
+end
+
+function M.intersects(center_a, half_a, center_b, half_b)
+	return math.abs(center_a.x - center_b.x) < (half_a.x + half_b.x)
+		and math.abs(center_a.y - center_b.y) < (half_a.y + half_b.y)
+end
+
+function M.draw_box(center, half_extents, color)
+	local left = center.x - half_extents.x
+	local right = center.x + half_extents.x
+	local bottom = center.y - half_extents.y
+	local top = center.y + half_extents.y
+	local z = center.z
+
+	local bottom_left = vmath.vector3(left, bottom, z)
+	local bottom_right = vmath.vector3(right, bottom, z)
+	local top_left = vmath.vector3(left, top, z)
+	local top_right = vmath.vector3(right, top, z)
+
+	draw_line(bottom_left, bottom_right, color)
+	draw_line(bottom_right, top_right, color)
+	draw_line(top_right, top_left, color)
+	draw_line(top_left, bottom_left, color)
+
+	draw_line(bottom_left, top_right, color)
+	draw_line(top_left, bottom_right, color)
+end
+
+return M
---
+++ main/debug_box/debug_box.go
@@ -0,0 +1,4 @@
+components {
+  id: "debug_box"
+  component: "/main/debug_box/debug_box.script"
+}
